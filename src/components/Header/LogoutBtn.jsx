58b744212693c7255baa1d43d44584e9bb723068
Ayushi Maithani <ayushimaithani04@gmail.com>
2024-08-12 19:15:00 +0530

    initial commit

---
+++ src/components/Header/LogoutBtn.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useDispatch } from 'react-redux' 
+import authServices from '../../appwrite/config'  
+import { useHistory } from "../../store/authSlice"
+
+function LogoutBtn() {
+    const dispatch = useDispatch()
+    const logoutHandler
+  return (
+    <div>LogoutBtn</div>
+  )
+}
+
+export default LogoutBtn
